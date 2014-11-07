f445b7e6082b4d827f60dc31e36c433ec1172db2
Emlyn Clay <eclay101@gmail.com>
2014-11-07 10:29:13 +0000

    Remove local from git

--- mayan/settings/local.py
+++ mayan/settings/local.py
@@ -3,7 +3,10 @@ from .base import *
 
 SECRET_KEY = '3el#pi(z42%2(isbg+e-v#5!#us!k_zd*j+zkuyrqd!3oy%(*_'
 
-# available IPs — needed for debug tag!
+# available IPs - needed for debug tag!
 INTERNAL_IPS = [
     '127.0.0.1'
 ]
+
+PROJECT_NAME = 'peptinnovate'
+PROJECT_TITLE = 'Peptinnovate'
