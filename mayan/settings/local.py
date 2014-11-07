7ae4ac11b914283356af18454c589708477d873c
Emlyn Clay <eclay101@gmail.com>
2014-11-07 10:21:36 +0000

    Set a default IP for local settings, required for debug tag

--- mayan/settings/local.py
+++ mayan/settings/local.py
@@ -2,3 +2,8 @@ from __future__ import absolute_import
 from .base import *
 
 SECRET_KEY = '3el#pi(z42%2(isbg+e-v#5!#us!k_zd*j+zkuyrqd!3oy%(*_'
+
+# available IPs — needed for debug tag!
+INTERNAL_IPS = [
+    '127.0.0.1'
+]
